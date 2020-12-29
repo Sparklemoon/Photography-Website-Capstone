ec47837ee914ba3accbd96f6885a02af9f632abb
Myra James <Treesmellgood@gmail.com>
2020-12-28 20:05:04 -0500

    started auth and login unfinished

---
+++ src/components/auth.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+export default class Auth extends  Component {
+    constructor(props) {
+        super(props);
+    }
+     render() {
+         return (
+             <div className="auth-page-wrapper">
+                 
+             </div>
+         )
+     }
+}
