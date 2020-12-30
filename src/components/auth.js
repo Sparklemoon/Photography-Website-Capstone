8a568a36896cb576e769104e0754710b0b0c49e4
Myra James <Treesmellgood@gmail.com>
2020-12-29 19:30:48 -0500

    fixed auth, login, and navbar

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,14 +1,43 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import loginImg from "../../../static/assets/images/auth/login.jpg";
+import Login from "./login.js";
 
-export default class Auth extends  Component {
-    constructor(props) {
-        super(props);
-    }
-     render() {
-         return (
-             <div className="auth-page-wrapper">
-                 
-             </div>
-         )
-     }
+
+export default class Auth extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleSuccessfullAuth = this.handleSuccessfullAuth.bind(this);
+    this.handleUnsuccessfullAuth = this.handleUnsuccessfullAuth.bind(this);
+  }
+
+  handleSuccessfullAuth() {
+    this.props.handleSuccessfullLogin();
+    this.props.history.push("/");
+  }
+
+  handleUnsuccessfullAuth() {
+    console.log(this.props)
+    this.props.handleUnsuccessfullLogin();
+  }
+
+  render() {
+    return (
+      <div className="auth-page-wrapper">
+        <div
+          className="left-column"
+          style={{
+            backgroundImage: `url(${loginImg})`
+          }}
+        />
+
+        <div className="right-column">
+          <Login
+            handleSuccessfulAuth={this.handleSuccessfullAuth}
+            handleUnsuccessfulAuth={this.handleUnsuccessfullAuth}
+          />
+        </div>
+      </div>
+    );
+  }
 }
--- src/components/login.js
+++ src/components/login.js
@@ -1,4 +1,6 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+
 
 export default class Login extends Component {
     constructor(props) {
@@ -9,5 +11,79 @@ export default class Login extends Component {
             password: "",
             errorText: ""
         };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value,
+            errorText: ""
+        });
+    }
+
+    handleSubmit(event) {
+        axios.post("",
+        {
+          client: {
+              email: this.state.email,
+              password: this.state.password
+          }  
+        },
+        { withCredentials: true }
+      )
+        .then(response => {
+          if (response.data.status === 'created') {
+             this.props.handleSuccessfullAuth();
+          } else {
+              this.setState({
+                  errorText: "Wrong email or password"
+              });
+              this.props.handleUnsuccessfullAuth();
+          }
+        })
+        // .catch(error => {
+        //     this.setState({
+        //         errorText: "An error occured"
+        //     });
+        //     this.props.handleUnsuccessfullAuth();
+        // });
+        
+    
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
+
+                <div>{this.state.errorText}</div>
+               
+                <form onSubmit={this.handleSubmit}>
+                    <input 
+                      type="email"
+                      name="email"
+                      placeholder="Your email"
+                      value={this.state.email}
+                      onChange={this.handleChange}
+                    />
+
+                    <input 
+                      type="password"
+                      name="password"
+                      placeholder="Your password"
+                      value={this.state.password}
+                      onChange={this.handleChange}
+                     />
+
+                    <div>
+                       <button type="submit">Login</button> 
+                    </div>
+                    
+                </form>
+            </div>
+        );
     }
 }
